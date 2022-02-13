d3924d9a2396185a440792a37bc392610fe516d6
irlaandrade <paula.irla@gmail.com>
2022-02-13 17:11:02 -0300

    initial commit

---
+++ src/main/java/com/implementingCollections/implementingCollections/ImplementingCollectionsApplication.java
@@ -0,0 +1,13 @@
+package com.implementingCollections.implementingCollections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplementingCollectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImplementingCollectionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/implementingCollections/implementingCollections/ImplementingCollectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.implementingCollections.implementingCollections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplementingCollectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
